39f348c80c1c7a2ec1c6bfd179ec993388b10bf7
Itamar Almog <itamaralmoghermel@gmail.con>
2023-10-05 14:55:48 +0300

    API basket function deco checkout not finished 13-14

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Pagination } from './shared/models/pagination';
-import { Product } from './shared/models/product';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +8,11 @@ import { Product } from './shared/models/product';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: Product[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private basketService: BasketService) {}
 
   ngOnInit(): void {
-    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
-      next: response => this.products = response.data, // what to do next
-      error: error => console.log(error), // what to do if there is an error
-      complete: () => {
-        console.log('request completed');
-        console.log('extra statment');
-      }
-    })
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) this.basketService.getBasket(basketId);
   }
 }
---
+++ Client/src/app/shared/models/basket.ts
@@ -0,0 +1,28 @@
+import * as cuid from 'cuid';
+// import {v4 as uuidv4 } from 'uuid';
+
+export interface BasketItem {
+    id: number;
+    productName: string;
+    price: number;
+    quantity: number;
+    pictureUrl: string;
+    brand: string;
+    type: string;
+}
+
+export interface Basket {
+    id: string;
+    items: BasketItem[];
+}
+
+export class Basket implements Basket {
+    id = cuid();
+    items: BasketItem[] = [];
+}
+
+export interface BasketTotals {
+    shipping: number;
+    subtotal: number;
+    total: number;
+}
